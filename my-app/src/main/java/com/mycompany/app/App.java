cdbfa87298a8b30544c317aa0fde11e3c57e3720
sunandini vutturi <vutturisunandini@gmail.com>
2024-12-03 14:53:46 +0000

    App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,30 @@
-package com.mycompany.app;
+#package com.mycompany.app;
 
 /**
  * Hello world!, Welcome to Devops Training!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+#public class App 
+#{
+#    public static void main( String[] args )
+#   {
+#       System.out.println( "Hello World!" );
+#   }
+#}
+package com.mycompany.app;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        // Keep the application running indefinitely
+        while (true) {
+            try {
+                Thread.sleep(30000);  // Sleep for 10 seconds to simulate a long-running process
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
+
