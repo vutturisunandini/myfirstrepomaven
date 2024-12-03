0bf4c5564b13e6108f02e23cd5f6aba6c7963d43
sunandini vutturi <vutturisunandini@gmail.com>
2024-12-03 14:58:23 +0000

    modified

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,16 +1,3 @@
-#package com.mycompany.app;
-
-/**
- * Hello world!, Welcome to Devops Training!
- *
- */
-#public class App 
-#{
-#    public static void main( String[] args )
-#   {
-#       System.out.println( "Hello World!" );
-#   }
-#}
 package com.mycompany.app;
 
 public class App {
